751efaa8a89376c963cf9799c957f18a68292411
LDumberger <lukas.dumberger@gmail.com>
2024-03-15 11:27:22 +0100

    make file path os agnostic

--- src/collect-traffic-data.py
+++ src/collect-traffic-data.py
@@ -2,6 +2,7 @@ import json
 import requests
 from datetime import datetime
 from datetime import timedelta
+import os
 
 traffic_sensor_group_identifier = 'd18db740-d364-43ff-86c7-9655d009a529'
 
@@ -17,7 +18,7 @@ print("Response status: ", response.status_code)
 
 data = response.json()
 
-fileName = 'data/{0}_{1}_Traffic.json'.format(todayMinus7Days, today)
+fileName = os.path.join('data', '{0}_{1}_Traffic.json'.format(todayMinus7Days, today))
 
 with open(fileName, 'w') as file:
     json.dump(data, file, indent=2)
